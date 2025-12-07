f6e090788649c26cbd320d416985d73e82d80ccb
YlliDr <dr.mr.ylli@gmail.com>
2025-12-07 22:34:51 +0100

    Some transitions added for header icons

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
       const profileBtn = document.getElementById("profile-btn");
       const profileMenu = document.getElementById("profile-menu");
 
-      // Toggle menu on click
+
       profileBtn.addEventListener("click", (e) => {
-        e.stopPropagation(); // prevent immediate document click
+        e.stopPropagation();
         profileMenu.classList.toggle("active");
       });
 
-      // Close menu when clicking outside
+
       document.addEventListener("click", (e) => {
         if (!profileBtn.contains(e.target) && !profileMenu.contains(e.target)) {
           profileMenu.classList.remove("active");
@@ -24,7 +24,6 @@
     btn.addEventListener("click", () => {
       hiddenCards.forEach(card => card.classList.toggle("hidden"));
 
-      // Update button text
       if (btn.textContent === "Show more") {
         btn.textContent = "Show less";
       } else {
