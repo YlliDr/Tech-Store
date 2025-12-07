16f9f729499e5d9f118bc18a8bf4699e61b4568c
YlliDr <dr.mr.ylli@gmail.com>
2025-12-07 22:02:08 +0100

    Anchor link added!

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const profileBtn = document.getElementById("profile-btn");
-const profileMenu = document.getElementById("profile-menu");
 
-// Toggle menu on click
-profileBtn.addEventListener("click", (e) => {
-  e.stopPropagation(); // prevent immediate document click
-  profileMenu.classList.toggle("active");
-});
+// Login dropdown
+      const profileBtn = document.getElementById("profile-btn");
+      const profileMenu = document.getElementById("profile-menu");
 
-// Close menu when clicking outside
-document.addEventListener("click", (e) => {
-  if (!profileBtn.contains(e.target) && !profileMenu.contains(e.target)) {
-    profileMenu.classList.remove("active");
-  }
-});
+      // Toggle menu on click
+      profileBtn.addEventListener("click", (e) => {
+        e.stopPropagation(); // prevent immediate document click
+        profileMenu.classList.toggle("active");
+      });
+
+      // Close menu when clicking outside
+      document.addEventListener("click", (e) => {
+        if (!profileBtn.contains(e.target) && !profileMenu.contains(e.target)) {
+          profileMenu.classList.remove("active");
+        }
+      });
 
     const btn = document.getElementById("toggleVisibility");
     const hiddenCards = document.querySelectorAll(".card.hidden");
+
+  // Show more products feature
 
     btn.addEventListener("click", () => {
       hiddenCards.forEach(card => card.classList.toggle("hidden"));
@@ -27,3 +31,12 @@ document.addEventListener("click", (e) => {
         btn.textContent = "Show more";
       }
     });
+
+  // Anchor link
+
+    document.getElementById("scrollToTopBtn").addEventListener("click", () => {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        });
