0db1351a8c6cb2d8d241a2ef2825220203f1ca4d
YlliDr <134789747+YlliDr@users.noreply.github.com>
2025-12-13 02:03:14 +0100

    Name when logged in showes

--- layout.js
+++ layout.js
@@ -32,7 +32,7 @@ function initHeaderLogic() {
     }
   });
 
-  // login state
+  // login state buttons
   const loginBtn = document.getElementById("login-btn");
   const signupBtn = document.getElementById("signup-btn");
   const logoutBtn = document.getElementById("logout-btn");
@@ -40,15 +40,23 @@ function initHeaderLogic() {
   const currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
 
   if (currentUser) {
+    // hide login/signup, show logout
     loginBtn.style.display = "none";
     signupBtn.style.display = "none";
     logoutBtn.style.display = "inline-block";
+
+
+    profileBtn.innerHTML = `<i class="fa-solid fa-user"></i><span>Hi, ${currentUser.fullName}! </span>`;
     profileBtn.title = `Logged in as ${currentUser.fullName}`;
   } else {
+
     loginBtn.style.display = "inline-block";
     signupBtn.style.display = "inline-block";
     logoutBtn.style.display = "none";
+    profileBtn.innerHTML = `<i class="fa-solid fa-user"></i>`;
+    profileBtn.title = "User menu";
   }
+
 
   logoutBtn.addEventListener("click", () => {
     sessionStorage.removeItem("currentUser");
